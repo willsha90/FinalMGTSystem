ea34d280632539b51876bbef4748605919bfbfd7
Will <shaheenwill@gmail.com>
2021-05-18 20:56:35 -0400

    updated seeding

--- 02-Homework/db/seed.sql
+++ 02-Homework/db/seed.sql
@@ -11,23 +11,23 @@ VALUES
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
+    ('Sales Lead', 15400, 1),
+    ('Salesperson', 30000, 1),
+    ('Lead Engineer', 155660, 2),
+    ('Software Engineer', 88000, 2),
+    ('Account Manager', 47000, 3),
     ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('Legal Team Lead', 30000, 4),
+    ('Lawyer', 87000, 4);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Betty', 'White', 1, NULL),
+    ('Jim', 'Chandler', 2, 1),
+    ('Gus', 'Gleason', 3, NULL),
+    ('Kevin', 'James', 4, 3),
+    ('Brian', 'Kemp', 5, NULL),
+    ('Smith', 'Lord', 6, 5),
+    ('Theirry', 'Henry', 7, NULL),
+    ('Allen', 'Brady', 8, 7);
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # FinalMGTSystem
 
- * Add departments, roles, employees
+ The purpose of this application was to create a functionality to organize, view, edit and add/remove several key aspects of an Employee Management System. Javascript, SQL, MySQL, the Utility and Inquirer modules were all utilized for this application.
 
-  * View departments, roles ADD, employees
-
-  * Update employee roles
+ I created a dataset in MYSQL that broke out employees, managers, roles and departments in a given population. Having connected VS Code/JS to MYSQL, I was able to write functionalities via join statements that allow the user to view the data in numerous fashions. These include viewing  
